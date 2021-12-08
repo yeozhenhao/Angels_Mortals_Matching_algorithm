5f9aebc071d8b3bf6d0aed2b5405cba24fee61b4
yeozhenhao <elgeneyeo@hotmail.com>
2021-12-08 12:32:49 +0800

    Updated matching algorithm

--- angel.py
+++ angel.py
@@ -161,7 +161,7 @@ def modify_player_list(player_list):
                 player.genderpref = GENDER_MALE
 
 
-def write_to_csv(index, *player_lists):
+def write_to_csv(index, name01, *player_lists):
     '''
     Writes a variable number of player lists to csv
     '''
@@ -169,19 +169,20 @@ def write_to_csv(index, *player_lists):
         if player_list is not None:
             print (f"Length of list: {len(player_list)}")
             cur_time = time.strftime("%Y-%m-%d %H-%M-%S")
-            with open(f"{index} - {cur_time}.csv", 'w', newline='') as f: ##In Python 3, if do not put newline='' AND choose 'w' instead of 'wb', you will have an empty 2nd row in output .csv file.
+            with open(f"{index} - {name01} - {cur_time}.csv", 'w', newline='') as f: ##In Python 3, if do not put newline='' AND choose 'w' instead of 'wb', you will have an empty 2nd row in output .csv file.
                 writer = csv.writer(f, delimiter=',')
                 header = ['Telegram Username','Name','GenderPref','Gender','Interests','2truths1lie','Intro','House','CG','Year','Faculty'] ##add header to output csv file
                 writer.writerow(i for i in header)
                 for player in player_list:
                     f.write(player.to_csv_row())
                     f.write("\n")
-            # write the first player again to close the loop
-                f.write(player_list[0].to_csv_row())
-                f.write("\n")
+            # # write the first player again to close the loop
+            #     f.write(player_list[0].to_csv_row())
+            #     f.write("\n")
                 f.close()
 
-
+def Difference_operator_lists(li1, li2):                            ##Used to find out the rejected players
+    return list(set(li1) - set(li2)) + list(set(li2) - set(li1))
 
 if __name__ == "__main__":
     print (f"\n\n")
@@ -199,4 +200,7 @@ if __name__ == "__main__":
     # Write each chain to a separate csv
     print("done")
     for index, player_chain in enumerate(list_of_player_chains):
-         write_to_csv(index, player_chain)
+        write_to_csv(index, "accepted", player_chain)
+        # creating csv list of rejected players
+        rejected_players_list = Difference_operator_lists(player_list, player_chain)
+        write_to_csv(index, "rejected", rejected_players_list)
